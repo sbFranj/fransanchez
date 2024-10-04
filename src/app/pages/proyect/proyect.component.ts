20d423c6fde0b3d332fb7b51b9c6ffc4d057eefa
Fran Sánchez <115450163+sbFranj@users.noreply.github.com>
2024-10-04 15:13:11 +0200

    Actualizacion
    nuevo proyecto, my app, cambio de descripcion home, justify de proyectos.

--- src/app/pages/proyect/proyect.component.ts
+++ src/app/pages/proyect/proyect.component.ts
@@ -14,64 +14,72 @@ export class ProyectComponent {
 
   constructor(public languageService: LanguageService) { }
 
+  video:boolean =false;
 
   register() {
     window.open("https://flicksense.vercel.app/auth/register", '_blank')
   }
 
   catalog(){
-    
-    if (!this.languageService.eng) {
-      Swal.fire({
-        icon: "warning",
-        title: "La pagina web está hospedada con una suscripcion gratuita, la primera cargar puede demorarse unos 30s~40s",
-        iconColor: "#efd558",
-        confirmButtonColor: "#7a0000",
-        width:"300px",
+    window.open("https://flicksense.vercel.app/movies", '_blank')
+    // if (!this.languageService.eng) {
+    //   Swal.fire({
+    //     icon: "warning",
+    //     title: "La pagina web está hospedada con una suscripcion gratuita, la primera cargar puede demorarse unos 30s~40s",
+    //     iconColor: "#efd558",
+    //     confirmButtonColor: "#7a0000",
+    //     width:"300px",
       
-      }).then((resp) => {
+    //   }).then((resp) => {
 
-        window.open("https://flicksense.vercel.app/movies", '_blank')
-      })
-    } else {
-      Swal.fire({
-        icon: "warning",
-        title: "The website is hosted with a free subscription, so the initial load may take about 30 to 40 seconds.",
-        iconColor: "#efd558",
-        confirmButtonColor: "#7a0000",
-        width:"300px"
-      }).then((resp) => {
+    //     window.open("https://flicksense.vercel.app/movies", '_blank')
+    //   })
+    // } else {
+    //   Swal.fire({
+    //     icon: "warning",
+    //     title: "The website is hosted with a free subscription, so the initial load may take about 30 to 40 seconds.",
+    //     iconColor: "#efd558",
+    //     confirmButtonColor: "#7a0000",
+    //     width:"300px"
+    //   }).then((resp) => {
 
-      window.open("https://flicksense.vercel.app/movies", '_blank')
-      })
-    }
+    //   window.open("https://flicksense.vercel.app/movies", '_blank')
+    //   })
+    // }
   }
 
-  goTo() {
-    if (!this.languageService.eng) {
-      Swal.fire({
-        icon: "warning",
-        title: "La pagina web está hospedada con una suscripcion gratuita, la primera cargar puede demorarse unos 30s~40s",
-        iconColor: "#efd558",
-        confirmButtonColor: "#7a0000",
-        width:"300px"
-      }).then((resp) => {
+  goTo(url:string) {
+    window.open(url, '_blank')
+    // if (!this.languageService.eng) {
+    //   Swal.fire({
+    //     icon: "warning",
+    //     title: "La pagina web está hospedada con una suscripcion gratuita, la primera cargar puede demorarse unos 30s~40s",
+    //     iconColor: "#efd558",
+    //     confirmButtonColor: "#7a0000",
+    //     width:"300px"
+    //   }).then((resp) => {
 
-        window.open("https://flicksense.vercel.app/", '_blank')
-      })
-    } else {
-      Swal.fire({
-        icon: "warning",
-        title: "The website is hosted with a free subscription, so the initial load may take about 30 to 40 seconds.",
-        iconColor: "#efd558",
-        confirmButtonColor: "#7a0000",
-        width:"300px"
-      }).then((resp) => {
+    //     window.open("https://flicksense.vercel.app/", '_blank')
+    //   })
+    // } else {
+    //   Swal.fire({
+    //     icon: "warning",
+    //     title: "The website is hosted with a free subscription, so the initial load may take about 30 to 40 seconds.",
+    //     iconColor: "#efd558",
+    //     confirmButtonColor: "#7a0000",
+    //     width:"300px"
+    //   }).then((resp) => {
 
-        window.open("https://flicksense.vercel.app/", '_blank')
-      })
-    }
-
-
+    //     window.open("https://flicksense.vercel.app/", '_blank')
+    //   })
+    // }
   }
+
+  verVideo(){
+    this.video = !this.video
+    setTimeout(()=>{
+      this.video = false;
+    },55000)
+  }
+
 }
