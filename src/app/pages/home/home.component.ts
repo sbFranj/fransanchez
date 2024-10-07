fe2c38aac1d6ca47aaa3152aabaee782c676dbff
Fran Sánchez <115450163+sbFranj@users.noreply.github.com>
2024-10-07 14:16:47 +0200

    ajustes de responsividad

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { LanguageService } from '../../services/language.service';
+import { AcademicComponent } from '../academic/academic.component';
+import { ProfessionalComponent } from '../professional/professional.component';
+import { ProyectComponent } from '../proyect/proyect.component';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-home',
